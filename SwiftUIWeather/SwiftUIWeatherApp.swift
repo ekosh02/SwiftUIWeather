6d283599a02aefce351a5a1e63e7d72eafdfc09d
ekosh02 <turapbaeveldos@gmail.com>
2023-12-12 00:48:28 +0600

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Yeldos Turapbayev on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
